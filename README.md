11c1c36583afba44930014c1558976a1b5083ef0
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-02-11 01:21:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# color_picker
+
+Wanna pick a color? Then pick this library.
+
+# Features
+
+- Support multiple color spaces.
+- Works on all platforms.
+- Figma-like UX.
---
+++ example/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:color_picker/color_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: ColorPicker(),
+          ),
+        ),
+      ),
+    );
+  }
+}
