7e3f66ca81787adbabb8417e7d2158e6c29b0577
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-02-11 01:46:26 +0200

    feat: add hex color support

--- lib/src/color_picker.dart
+++ lib/src/color_picker.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ColorPicker extends StatelessWidget {
   const ColorPicker({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const TextField();
+    return TextFormField(
+      maxLength: 6,
+      initialValue: 'FFFFFF',
+      decoration: const InputDecoration(
+        prefixText: '#',
+        counterText: '',
+      ),
+      inputFormatters: [
+        // TODO: allow all inputs and fallback to current value if invalid
+        FilteringTextInputFormatter.allow(
+          RegExp(r'[0-9a-fA-F]'),
+        ),
+      ],
+    );
   }
 }
