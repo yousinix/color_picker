6ec23d4fb19a40eb5c489b652bef104da0d6491c
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-02-11 22:51:22 +0200

    feat: add color space picker

--- lib/src/color_picker.dart
+++ lib/src/color_picker.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'color_utils.dart';
 
+enum ColorSpace {
+  hex('HEX');
+
+  const ColorSpace(this.name);
+  final String name;
+}
+
 class ColorPicker extends StatefulWidget {
   final Color initialColor;
+  final ColorSpace initialColorSpace;
   final ValueChanged<Color>? onChange;
 
   const ColorPicker({
     super.key,
     required this.initialColor,
+    this.initialColorSpace = ColorSpace.hex,
     this.onChange,
   });
 
@@ -17,12 +26,14 @@ class ColorPicker extends StatefulWidget {
 }
 
 class _ColorPickerState extends State<ColorPicker> {
+  late ColorSpace space;
   late Color color;
   late int alpha;
 
   @override
   void initState() {
     super.initState();
+    space = widget.initialColorSpace;
     color = widget.initialColor;
     alpha = 100;
   }
@@ -44,6 +55,25 @@ class _ColorPickerState extends State<ColorPicker> {
           height: 24,
           width: 24,
           color: color,
+        ),
+        const SizedBox.square(
+          dimension: 8,
+        ),
+        Flexible(
+          flex: 1,
+          child: DropdownButton(
+            value: widget.initialColorSpace,
+            items: ColorSpace.values
+                .map((x) => DropdownMenuItem(
+                      value: x,
+                      child: Text(x.name),
+                    ))
+                .toList(),
+            onChanged: (value) {
+              if (value == null) return;
+              setState(() => space = value);
+            },
+          ),
         ),
         const SizedBox.square(
           dimension: 8,
