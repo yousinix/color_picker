1b1f62b8290fb95c28b528367a5dab647a626742
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-02-11 02:19:48 +0200

    feat: add color preview

--- lib/src/color_picker.dart
+++ lib/src/color_picker.dart
@@ -2,30 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'color_utils.dart';
 
-class ColorPicker extends StatelessWidget {
+class ColorPicker extends StatefulWidget {
+  static const initialColor = Color(0xFF00FF00);
   final ValueChanged<Color>? onChange;
 
   const ColorPicker({super.key, this.onChange});
 
   @override
+  State<ColorPicker> createState() => _ColorPickerState();
+}
+
+class _ColorPickerState extends State<ColorPicker> {
+  late Color color;
+
+  @override
+  void initState() {
+    super.initState();
+    color = ColorPicker.initialColor;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      maxLength: 6,
-      initialValue: 'FFFFFF',
-      decoration: const InputDecoration(
-        prefixText: '#',
-        counterText: '',
-      ),
-      inputFormatters: [
-        // TODO: allow all inputs and fallback to current value if invalid
-        FilteringTextInputFormatter.allow(
-          RegExp(r'[0-9a-fA-F]'),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: 24,
+          width: 24,
+          color: color,
+
+        ),
+        const SizedBox.square(
+          dimension: 8,
+        ),
+        Expanded(
+          child: TextFormField(
+            maxLength: 6,
+            initialValue: ColorPicker.initialColor.toHex(
+              leadingHashSign: false,
+            ),
+            decoration: const InputDecoration(
+              prefixText: '#',
+              counterText: '',
+            ),
+            inputFormatters: [
+              // TODO: allow all inputs and fallback to current value if invalid
+              FilteringTextInputFormatter.allow(
+                RegExp(r'[0-9a-fA-F]'),
+              ),
+            ],
+            onChanged: (value) {
+              if (value.length != 6) return;
+              final newColor = ColorUtils.fromHex(value);
+
+              widget.onChange?.call(newColor);
+              setState(() => color = newColor);
+            },
+          ),
         ),
       ],
-      onChanged: (value) {
-        if (value.length != 6) return;
-        onChange?.call(ColorUtils.fromHex(value));
-      },
     );
   }
 }
--- lib/src/color_utils.dart
+++ lib/src/color_utils.dart
@@ -5,4 +5,10 @@ extension ColorUtils on Color {
     final intValue = int.parse('0xFF$value');
     return Color(intValue);
   }
+
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
